f8d145e6ed030c7cb8006dc1a6134e7226422920
Ross Khapilov <rossxpanteley@gmail.com>
2021-10-03 13:24:06 +0300

    Написал виджеты с кастомным текстом и иконкой, которые через определенную задержку переключают стиль текста, иконку

---
+++ lib/widgets/animated90s/animated_90s_icon.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+/// Класс анимированной иконки, которая переключается между элементами списка [_iconsList]
+/// с заданной задержкой [_duration]
+class AnimatedIcon90s extends StatefulWidget {
+  const AnimatedIcon90s({
+    required List<IconData> iconsList,
+    Duration duration = const Duration(milliseconds: 100),
+    Key? key,
+  })  : _iconsList = iconsList,
+        _duration = duration,
+        super(key: key);
+
+  /// Список допустимых иконок
+  final List<IconData> _iconsList;
+
+  /// Задержка переключения анимации между элементами [iconList]
+  final Duration _duration;
+
+  @override
+  _AnimatedIcon90sState createState() => _AnimatedIcon90sState();
+}
+
+class _AnimatedIcon90sState extends State<AnimatedIcon90s>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  final _rnd = Random();
+
+  /// Индекс элемента из списка конфигурации
+  var _oldIndex = 0;
+
+  /// Объект слушателя, по изменению которого обновляется отображение
+  late final _notifier = ValueNotifier<IconData>(_getIcon());
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget._duration,
+      vsync: this,
+    );
+    // Запуск цикла анимации
+    _repeater();
+  }
+
+  @override
+  void dispose() {
+    _controller
+      ..stop()
+      ..dispose();
+    super.dispose();
+  }
+
+  /// Метод перезапуска анимации по ее окончанию
+  void _repeater() {
+    _controller.forward(from: 0).whenComplete(() {
+      _notifier.value = _getIcon();
+      _repeater();
+    });
+  }
+
+  /// Возвращает случайную иконку из массива конфигурации
+  IconData _getIcon() {
+    int newIndex;
+    do {
+      newIndex = _rnd.nextInt(widget._iconsList.length);
+    } while (newIndex == _oldIndex);
+    _oldIndex = newIndex;
+
+    return widget._iconsList[newIndex];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: ValueListenableBuilder<IconData>(
+        valueListenable: _notifier,
+        builder: (_, value, __) => Icon(value),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/animated90s/animated_90s_text.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AnimatedText90s extends StatefulWidget {
+  const AnimatedText90s(
+    this.text, {
+    this.duration = const Duration(milliseconds: 250),
+    this.fontFamilies = const [
+      'pepega font',
+      'A Childish Wonders',
+    ],
+    Key? key,
+  }) : super(key: key);
+
+  final String text;
+  final Duration duration;
+  final List<String> fontFamilies;
+
+  @override
+  _AnimatedText90sState createState() => _AnimatedText90sState();
+}
+
+class _AnimatedText90sState extends State<AnimatedText90s> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  final _rnd = Random();
+  var _oldIndex = 0;
+
+  late TextStyle style = TextStyle(
+    fontFamily: widget.fontFamilies.first,
+    fontFeatures: const [FontFeature.randomize()],
+  );
+
+  late final _notifier = ValueNotifier(_getStyle());
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+    _repeater();
+  }
+
+  @override
+  void dispose() {
+    _controller
+      ..stop
+      ..dispose();
+    super.dispose();
+  }
+
+  TextStyle _getStyle() {
+    int newIndex;
+    do {
+      newIndex = _rnd.nextInt(widget.fontFamilies.length);
+    } while (newIndex == _oldIndex);
+    _oldIndex = newIndex;
+    return style.copyWith(fontFamily: widget.fontFamilies[newIndex]);
+  }
+
+  /// Метод перезапуска анимации по ее окончанию
+  void _repeater() {
+    _controller.forward(from: 0).whenComplete(() {
+      _notifier.value = _getStyle();
+      _repeater();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: ValueListenableBuilder<TextStyle>(
+        valueListenable: _notifier,
+        builder: (_, value, __) => Text(
+          widget.text,
+          style: value,
+        ),
+      ),
+    );
+  }
+}
