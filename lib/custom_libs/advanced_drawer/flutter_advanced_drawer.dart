7295203658fef6d5786409737a442984b413413b
Ross <rossxpanteley@gmail.com>
2021-10-19 16:55:05 +0300

    Оригинальный код библиотеки без изменений https://pub.dev/packages/flutter_advanced_drawer Версия 1.2.4

---
+++ lib/custom_libs/advanced_drawer/flutter_advanced_drawer.dart
@@ -0,0 +1,10 @@
+/// Немного изменил исходники библиотеки https://pub.dev/packages/flutter_advanced_drawer
+/// чтобы подогнать ее под мои требования
+
+library flutter_advanced_drawer;
+
+import 'package:flutter/material.dart';
+
+part 'src/controller.dart';
+part 'src/value.dart';
+part 'src/widget.dart';
