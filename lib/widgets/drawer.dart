5faf0f2908f7ecd448a9a072e6042c9b3c67abfd
Ross <rossxpanteley@gmail.com>
2021-10-25 17:03:10 +0300

    Прокручиваемый контент в шторке

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -9,7 +9,7 @@ import 'package:shop_list/widgets/avatar.dart';
 
 /// Drawer в отдельном файле для лучшей читаемости и гибкости кода
 class AppDrawer extends StatelessWidget {
-  const AppDrawer({
+  AppDrawer({
     required this.advancedDrawerController,
     required this.child,
     this.backgroundColor = Colors.blueGrey,
@@ -22,6 +22,8 @@ class AppDrawer extends StatelessWidget {
   final Color backgroundColor;
 
   final Widget child;
+
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -49,48 +51,58 @@ class AppDrawer extends StatelessWidget {
         child: ListTileTheme(
           textColor: Colors.white,
           iconColor: Colors.white,
-          child: ConstrainedBox(
-            constraints: const BoxConstraints.tightFor(
-              width: 200,
-              height: double.infinity,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 34, bottom: 20),
-                  child: Obx(
-                    () => authController.firestoreUser.value != null
-                        ? AnimatedPainterCircleWithBorder90s(
-                            boxColor: backgroundColor,
-                            child: Avatar(user: authController.firestoreUser.value!),
-                          )
-                        : const SizedBox(),
+          // Для обеспечения корректной работы Flex/Spacer элемента использовал пример из документации
+          // https://api.flutter.dev/flutter/widgets/SingleChildScrollView-class.html
+          // где задается ограничение по высоте из аргумента LayoutBuilder [constrains.maxHeight]
+          child: LayoutBuilder(
+            builder: (context, constraints) => SingleChildScrollView(
+              controller: _scrollController,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: 200,
+                  minHeight: constraints.maxHeight,
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 34, bottom: 20),
+                        child: Obx(
+                          () => authController.firestoreUser.value != null
+                              ? AnimatedPainterCircleWithBorder90s(
+                                  boxColor: backgroundColor,
+                                  child: Avatar(user: authController.firestoreUser.value!),
+                                )
+                              : const SizedBox(),
+                        ),
+                      ),
+                      Obx(() => Text(
+                            '${authController.firestoreUser.value?.name}',
+                            style: titleTextStyle,
+                          )),
+                      const SizedBox(height: 50),
+                      ListTile(
+                        onTap: () {
+                          Get.toNamed('/account');
+                        },
+                        leading: const AnimatedIcon90s(iconsList: CustomIcons.user),
+                        title: const Text('Account'),
+                      ),
+                      ListTile(
+                        onTap: () {},
+                        leading: const AnimatedIcon90s(iconsList: CustomIcons.user),
+                        title: const Text('Settings'),
+                      ),
+                      const Spacer(),
+                      TextButton(
+                        onPressed: authController.signOut,
+                        child: const Text('Sign out'),
+                      ),
+                    ],
                   ),
                 ),
-                Text(
-                  '${authController.firestoreUser.value?.name}',
-                  style: titleTextStyle,
-                ),
-                const SizedBox(height: 50),
-                ListTile(
-                  onTap: () {
-                    Get.toNamed('/account');
-                  },
-                  leading: const AnimatedIcon90s(iconsList: CustomIcons.user),
-                  title: const Text('Account'),
-                ),
-                ListTile(
-                  onTap: () {},
-                  leading: const AnimatedIcon90s(iconsList: CustomIcons.user),
-                  title: const Text('Settings'),
-                ),
-                const Spacer(),
-                TextButton(
-                  onPressed: authController.signOut,
-                  child: const Text('Sign out'),
-                ),
-              ],
+              ),
             ),
           ),
         ),
