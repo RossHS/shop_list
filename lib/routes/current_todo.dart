1b5ea55679f21a893d1dce7f898b425ae820e181
Ross Khapilov <rossxpanteley@gmail.com>
2021-11-15 14:22:07 +0300

    Анимация закрытия задачи в маршруте "текущая задача"

--- lib/routes/current_todo.dart
+++ lib/routes/current_todo.dart
@@ -1,5 +1,8 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:shop_list/controllers/controllers.dart';
 import 'package:shop_list/custom_icons.dart';
 import 'package:shop_list/models/models.dart';
@@ -62,61 +65,134 @@ class CurrentTodo extends StatelessWidget {
 }
 
 /// Основное тело виджета при успешной загрузки модели списка дел
-class _LoadedWidget extends StatelessWidget {
+class _LoadedWidget extends StatefulWidget {
   const _LoadedWidget({Key? key}) : super(key: key);
+
+  @override
+  State<_LoadedWidget> createState() => _LoadedWidgetState();
+}
+
+class _LoadedWidgetState extends State<_LoadedWidget> with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final CurvedAnimation _crossLineAnimation;
+  late final CurvedAnimation _textInfoScaleAnimation;
+  late final Animation<double> _textInfoRotateAnimation;
+
+  /// Флаг для перезапуска анимации, т.е. если в другом месте изменили статус завершенности задачи, а мы поставим
+  /// его вновь на завершенный, мы захотим увидеть анимацию с 0, вот для этого этот флаг и существует.
+  /// Он следит за переключением статуса задачи
+  bool _isFirstTodoCompletedStatus = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _crossLineAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: const Interval(0.1, 0.6, curve: Curves.elasticOut),
+    );
+    _textInfoScaleAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: const Interval(0.5, 1, curve: Curves.elasticOut),
+    );
+    _textInfoRotateAnimation = Tween<double>(
+      begin: 0,
+      end: 2 * math.pi + math.pi / 6,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: const Interval(0.5, 1, curve: Curves.elasticOut),
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
     return GetBuilder<TodoViewController>(
-      builder: (controller) => Stack(
-        children: [
-          Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: controller.todoModel!.elements.length,
-                  itemBuilder: (context, index) {
-                    final element = controller.todoModel!.elements[index];
-                    return ListTile(
-                      // Используем uid в качестве ключа, а не сам элемент, так как при изменении
-                      // элемента (к примеру, смена статуса на выполнено) => ключ тоже меняется
-                      // и создается новый виджет.
-                      key: ValueKey(element.uid),
-                      onTap: () {
-                        _changeTodoElementCompleteStatus(element);
-                      },
-                      leading: Checkbox(
-                          value: element.completed,
-                          onChanged: (_) {
-                            _changeTodoElementCompleteStatus(element);
-                          }),
-                      title: Text(
-                        element.name,
-                        style: textTheme.bodyText2?.copyWith(
-                          decoration: element.completed ? TextDecoration.lineThrough : null,
+      builder: (controller) {
+        if (!controller.isTodoCompleted) {
+          _isFirstTodoCompletedStatus = true;
+        }
+        if (controller.isTodoCompleted && _isFirstTodoCompletedStatus) {
+          _isFirstTodoCompletedStatus = false;
+          _animationController.forward(from: 0);
+        }
+        return Stack(
+          children: [
+            Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: controller.todoModel!.elements.length,
+                    itemBuilder: (context, index) {
+                      final element = controller.todoModel!.elements[index];
+                      return ListTile(
+                        // Используем uid в качестве ключа, а не сам элемент, так как при изменении
+                        // элемента (к примеру, смена статуса на выполнено) => ключ тоже меняется
+                        // и создается новый виджет.
+                        key: ValueKey(element.uid),
+                        onTap: () {
+                          _changeTodoElementCompleteStatus(element);
+                        },
+                        leading: Checkbox(
+                            value: element.completed,
+                            onChanged: (_) {
+                              _changeTodoElementCompleteStatus(element);
+                            }),
+                        title: Text(
+                          element.name,
+                          style: textTheme.bodyText2?.copyWith(
+                            decoration: element.completed ? TextDecoration.lineThrough : null,
+                          ),
                         ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: AnimatedButton90s(
+                    onPressed: () => _completeTodoDialog(context),
+                    child: const Text('Завершить'),
+                  ),
+                )
+              ],
+            ),
+            if (controller.isTodoCompleted)
+              Positioned.fill(
+                child: CustomPaint(
+                  painter: _CustomPainterCrossLine(_crossLineAnimation),
+                  willChange: true,
+                  child: ScaleTransition(
+                    scale: _textInfoScaleAnimation,
+                    child: AnimatedBuilder(
+                      builder: (BuildContext context, Widget? child) {
+                        return Transform.rotate(
+                          angle: _textInfoRotateAnimation.value,
+                          child: child,
+                        );
+                      },
+                      animation: _textInfoRotateAnimation,
+                      child: Center(
+                        child: _CompletedInformation(),
                       ),
-                    );
-                  },
+                    ),
+                  ),
                 ),
               ),
-              // Если задача не закрыта, то отображаем кнопку для ее закрытия
-              !controller.isTodoCompleted
-                  ? Padding(
-                      padding: const EdgeInsets.all(24.0),
-                      child: AnimatedButton90s(
-                        onPressed: () => _completeTodoDialog(context),
-                        child: const Text('Завершить'),
-                      ),
-                    )
-                  : const SizedBox(),
-            ],
-          ),
-        ],
-      ),
+          ],
+        );
+      },
     );
   }
 
@@ -195,5 +271,74 @@ class _LoadedWidget extends StatelessWidget {
   void _changeTodoElementCompleteStatus(TodoElement element) {
     final controller = Get.find<TodoViewController>();
     controller.changeTodoElementCompleteStatus(isCompleted: !element.completed, todoElementUid: element.uid);
+  }
+}
+
+class _CustomPainterCrossLine extends CustomPainter {
+  _CustomPainterCrossLine(this.width) : super(repaint: width);
+
+  Animation<double> width;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..strokeWidth = 3
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawLine(
+      const Offset(0, 0),
+      Offset(width.value * size.width, size.height),
+      paint,
+    );
+
+    canvas.drawLine(
+      Offset(size.width, 0),
+      Offset(size.width - (width.value * size.width), size.height),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class _CompletedInformation extends StatelessWidget {
+  _CompletedInformation({Key? key}) : super(key: key);
+  final formatterDate = DateFormat('dd MM yyyy');
+  final formatterTime = DateFormat('HH:mm:ss');
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<TodoViewController>();
+    assert(controller.todoModel != null);
+
+    final completedDateTime = DateTime.fromMillisecondsSinceEpoch(controller.todoModel!.completedTimestamp);
+    return AnimatedPainterSquare90s(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'ЗАКРЫТО!',
+              style: Get.textTheme.headline5,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              formatterDate.format(completedDateTime),
+              style: TextStyle(fontSize: 15, color: Colors.black.withOpacity(0.3)),
+            ),
+            Text(
+              formatterTime.format(completedDateTime),
+              style: TextStyle(fontSize: 15, color: Colors.black.withOpacity(0.3)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
