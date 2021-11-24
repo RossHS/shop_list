cff1188374334ca2b25a403bda7bebbfdfd97377
Ross Khapilov <rossxpanteley@gmail.com>
2021-11-24 21:51:55 +0300

    Удалил неиспользуемый код. Сохранил текущий прогресс динамической смены темы

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shop_list/controllers/theme_controller.dart';
+import 'package:shop_list/custom_icons.dart';
 import 'package:shop_list/models/models.dart';
 import 'package:shop_list/widgets/animated90s/animated_90s.dart';
 
@@ -10,11 +11,31 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AnimatedAppBar90s(
-        title: Text('Настройки темы'),
-      ),
-      body: _Body(),
+    return GetBuilder<ThemeController>(
+      builder: (controller) {
+        // Obx - для перестроения темы маршрута. Нужен только здесь,
+        // так как в других частях приложения не будет динамически изменяться тема
+        return Obx(() {
+          // TODO 24.11.2021 придумать способ организации виджетов зависящих от темы. Скорее всего просто использовать фабрику, фабричный метод или что-то на подобии
+          PreferredSizeWidget appBar = controller.appTheme.value is Animated90sThemeData
+              ? AnimatedAppBar90s(
+                  title: const Text('Настройки темы'),
+                  leading: IconButton(
+                    onPressed: Get.back,
+                    icon: const AnimatedIcon90s(
+                      iconsList: CustomIcons.arrow,
+                    ),
+                  ),
+                )
+              : AppBar(
+                  title: const Text('Настройки темы'),
+                );
+          return Scaffold(
+            appBar: appBar,
+            body: const _Body(),
+          );
+        });
+      },
     );
   }
 }
