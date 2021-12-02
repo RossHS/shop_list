5ef6617c202fe6297a290d177fe27771e03b8041
Ross Khapilov <rossxpanteley@gmail.com>
2021-12-02 08:18:22 +0300

    Ночь разобрался в проблеме (описание в комментариях). Залил сырой вариант, чтобы продолжить работу в офисе

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:shop_list/controllers/info_overlay_controller.dart';
 import 'package:shop_list/controllers/theme_controller.dart';
 import 'package:shop_list/models/models.dart';
+import 'package:shop_list/widgets/animated90s/animated_90s_painter_square.dart';
 import 'package:shop_list/widgets/themes_factories/abstract_theme_factory.dart';
 
 /// Маршрут настройки тем приложения
@@ -91,6 +92,8 @@ class _Body extends StatelessWidget {
           ),
         ),
         const _ButtonsRow(),
+        const SizedBox(height: 20),
+        const _SpecificThemeSettings(),
       ],
     );
   }
@@ -133,6 +136,84 @@ class _ButtonsRow extends StatelessWidget {
                   child: const Text('Диалог')),
             ),
           ],
+        );
+      },
+    );
+  }
+}
+
+/// Панель с настройками для специфичных параметров тем, т.е. с каждой выбранной темой [ThemeDataWrapper],
+/// будет отображаться свой уникальный набор настроек
+/// TODO 02.12.2021 написать механизм настройки уникальных параметров тем!!!
+class _SpecificThemeSettings extends StatefulWidget {
+  const _SpecificThemeSettings({Key? key}) : super(key: key);
+
+  @override
+  State<_SpecificThemeSettings> createState() => _SpecificThemeSettingsState();
+}
+
+class _SpecificThemeSettingsState extends State<_SpecificThemeSettings> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GetX<ThemeController>(
+      builder: (controller) {
+        final themeFactory = ThemeFactory.instance(controller.appTheme.value);
+        return AnimatedSwitcher(
+          duration: Duration(seconds: 1),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return ScaleTransition(child: child, scale: animation);
+          },
+          // Использую метод buildWidget, а не определяю отдельный метод построения виджетов "настройки стиля"
+          // в абстрактной фабрике, так как тесты показали, что Get.theme отдает не актуальный объект [ThemeData],
+          // т.е. выглядит это так, что все "опаздывает" на один фрейм, к примеру, при установки текста в черный цвет
+          // (когда изначально был белый), цвет в данных полях останется белым, а при изменении цвета темы со светлой на темную,
+          // текст станет черным, а тема останется светлой, а при изменении следующего поля тема станет темной и т.д.
+          // (hot reload обновляет тему до актуальной/верной)
+          // Причем данная проблема наблюдается только на окне настройки, где необходим максимально быстрый отклик на изменения темы.
+          // Мне кажется, что проблема кроется в том, что Get.theme где-то под капотом использует future
+          // и я таким образом пролетаю до обновления ThemeData в Get.theme.
+          //
+          // Варианты решения.
+          // 1) Глубоко разобраться с устройством внутреннего строения алгоритма Get.theme, возможно добавить callback,
+          //  когда тема 100% обновилась, потом перестраивать нужные мне виджеты
+          // 2) Использовать актуальный ThemeData из BuildContext.
+          // 2.1) Переписать все методы абстрактной фабрики и передавать в каждый context в качестве аргумента.
+          // 2.2) Использовать метод themeFactory.buildWidget и только тут использовать BuildContext.
+          //  Или же создать отдельный виджеты, и не пользоваться фабрикой вовсе.
+          //
+          // Т.к. эта проблема беспокоит только лишь на окне настройки, то первый вариант отпадает из своей
+          // излишней сложности. 2.1. тоже можно отбросить по этой же причине, плюс понадобиться изменять уже существующий рабочий код,
+          // ради уникальной проблемы. Таким образом остается вариант 2.2, который я и реализовал.
+          child: themeFactory.buildWidget(
+            animated90s: (_) {
+              final config = (themeFactory.themeWrapper as Animated90sThemeDataWrapper)
+                  .paint90sConfig
+                  .copyWith(backgroundColor: theme.canvasColor);
+              return Padding(
+                key: ValueKey<String>(controller.appTheme.value.themePrefix),
+                padding: const EdgeInsets.all(10.0),
+                child: AnimatedPainterSquare90s(
+                  config: config,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Настройки стиля',
+                        style: theme.textTheme.headline5,
+                      ),
+                      Slider(
+                        value: 10,
+                        label: '10',
+                        min: 0,
+                        max: 100,
+                        onChanged: (double value) {},
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
         );
       },
     );
